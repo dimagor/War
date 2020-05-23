cd03c2981959fa5e12f49264f21cb25ac58ef336
YanHirsh <33080694+YanHirsh@users.noreply.github.com>
2020-05-23 11:43:57 -0400

    Update War.py

--- War.py
+++ War.py
@@ -2,50 +2,50 @@ import random
 
 deck_of_cards = [i for i in range(2,11) for num in range(4)]
 
-Tie_lst = []
+loser_deck = []
 
+#random.seed(42)
 random.shuffle(deck_of_cards)
-random.seed(42)
 
-class Player(): 
-    
-    def __init__(self, cards):
-        self.cards = cards
-              
-    def cut_deck(self):
-        if self == player1:
-            self.cards = [card for index, card in enumerate(deck_of_cards) if index % 2 == 0]
-        elif self == player2:
-            self.cards = [card for index, card in enumerate(deck_of_cards) if index % 2 == 1] 
+player1_cards = [card for index, card in enumerate(deck_of_cards) if index % 2 == 0]
+player2_cards = [card for index, card in enumerate(deck_of_cards) if index % 2 == 1] 
      
-player1 = Player([]) 
-player2 = Player([])    
+print(player1_cards, 'P1 cards')
+print(player2_cards, 'P2 cards')
+print('')
 
-def compare_cards():
-    while len(player1.cards) > 0 and len(player2.cards) > 0:    
-            print('Player 1 drew -> {}'.format(player1.cards[0]))
-            print('Player 2 drew -> {}'.format(player2.cards[0]))
-            if player1.cards[0] > player2.cards[0]:
-                player2.cards.append(player1.cards[0])
-                del player1.cards[0]
-                return 'Player 1 won the round!'
-            elif player2.cards[0] > player1.cards[0]:
-                player1.cards.append(player2.cards[0])
-                del player2.cards[0]
-                return 'Player 2 won the round!'
-            elif player1.cards[0] == player2.cards[0]:
-                tie_lst.append(player1.cards[0])
-                del player1.cards[0] and del player2.cards[0]
-                
-    else:
-        if len(player1.cards == 0):
-            return "Player 1 lost!"
-        elif len(player2.cards == 0):
-            return "Player 2 lost!"
-
-
-Player.cut_deck(player1)
-Player.cut_deck(player2)
-
-print(compare_cards())
-    
+while len(player1_cards) > 0 and len(player2_cards) > 0:    
+    print('Player 1 drew -> {}'.format(player1_cards[0]))
+    print('Player 2 drew -> {}'.format(player2_cards[0]))
+    if player1_cards[0] > player2_cards[0]:         
+        loser_deck.append(player1_cards[0]) 
+        loser_deck.append(player2_cards[0])
+        for i in loser_deck:             
+            player2_cards.append(i)                            
+        del player1_cards[0]
+        del player2_cards[0] 
+        #print(loser_deck, 'loser deck')
+        loser_deck = []               
+        print('Player 1 won the round!')
+    elif player2_cards[0] > player1_cards[0]:          
+        loser_deck.append(player2_cards[0])
+        loser_deck.append(player1_cards[0])
+        for i in loser_deck:
+            player1_cards.append(i)          
+        del player2_cards[0]   
+        del player1_cards[0]
+        #print(loser_deck, 'loser deck')            
+        loser_deck = []      
+        print('Player 2 won the round!')
+    elif player1_cards[0] == player2_cards[0]:
+        loser_deck.append(player1_cards[0])
+        loser_deck.append(player2_cards[0])
+        del player1_cards[0] 
+        del player2_cards[0]         
+        #print(loser_deck, 'loser deck')                  
+if len(player1_cards) == 0:
+    print('')
+    print("Player 2 won the game!")
+elif len(player2_cards) == 0:
+    print('')
+    print("Player 1 won the game!")
